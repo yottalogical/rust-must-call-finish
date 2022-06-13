d689e8863f8d3e612e5937add91f897aa98f6fa2
yottalogical <email@yottalogical.com>
2022-06-13 15:16:45 -0400

    Add sometimes_correct test

--- tests/examples.rs
+++ tests/examples.rs
@@ -14,3 +14,17 @@ fn incorrect() {
     let example = Example::new();
     example.do_something();
 }
+
+#[tokio::test]
+async fn sometimes_correct() {
+    // Change this to false for it to panic
+    // The fact it will/won't panic depending on a value is undesirable
+    static FINISH_CORRECTLY: bool = true;
+
+    let example = Example::new();
+    example.do_something();
+
+    if FINISH_CORRECTLY {
+        example.finish().await;
+    }
+}
