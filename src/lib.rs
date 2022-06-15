6eee5499560ee9e6dc29c9f1f61ffc8e96f906cb
yottalogical <email@yottalogical.com>
2022-06-15 09:37:49 -0400

    Block on async_drop if not already called

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::thread::panicking;
-
 pub struct Example {
     finished: bool,
 }
@@ -12,14 +10,17 @@ impl Example {
     pub fn do_something(&self) {}
 
     pub async fn finish(mut self) {
-        self.finished = true
+        self.async_drop().await;
+        self.finished = true;
     }
+
+    async fn async_drop(&mut self) {}
 }
 
 impl Drop for Example {
     fn drop(&mut self) {
-        if !self.finished && !panicking() {
-            panic!("Example must not be dropped implicitly. Instead call Example::finish.");
+        if !self.finished {
+            futures::executor::block_on(self.async_drop());
         }
     }
 }
--- tests/examples.rs
+++ tests/examples.rs
@@ -1,30 +1,14 @@
 use must_call_finish::Example;
 
 #[tokio::test]
-async fn correct() {
+async fn calls_finish() {
     let example = Example::new();
     example.do_something();
-
     example.finish().await;
 }
 
 #[test]
-#[should_panic]
-fn incorrect() {
+fn doesnt_call_finish() {
     let example = Example::new();
     example.do_something();
-}
-
-#[tokio::test]
-async fn sometimes_correct() {
-    // Change this to false for it to panic
-    // The fact it will/won't panic depending on a value is undesirable
-    static FINISH_CORRECTLY: bool = true;
-
-    let example = Example::new();
-    example.do_something();
-
-    if FINISH_CORRECTLY {
-        example.finish().await;
-    }
 }
